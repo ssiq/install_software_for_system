023c61aba1bdf33fc06e0190c65e34e56072e366
wlw <liweiwuhome@hotmail.com>
2015-11-26 18:18:11 +0800

    first

---
+++ install.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+#need set param
+scala_url=?
+scala_file_name=?
+conda_url=?
+conda_name=?
+
+bash_rc=~/.bashrc
+
+#solve the wifi problem
+echo "blacklist acer-wmi" | sudo tee -a /etc/modprobe.d/blacklist.conf
+
+#install need_thing for c++/c
+sudo apt-get install build-essential
+
+#install something for edit
+sudo apt-get install vim
+sudo add-apt-repository ppa:webupd8team/atom 
+sudo apt-get update
+sudo apt-get install atom
+
+#install master tool
+sudo apt-get install synapse
+sudo apt-get install ksnapshot
+
+#install java
+sudo apt-get install python-software-properties
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install oracle-java8-installer
+#need install idea by hand
+
+function add_to_bash_rc(){
+    for i in "$@";do
+        echo $i | tee -a $bash_rc
+    done
+}
+
+#install scala
+wget $scala_url
+tar zxf $scala_file_name.tgz
+sudo mv $scala_file_name /usr/share/$scala_file_name
+sudo ln -s /usr/share/$scala_file_name /usr/share/scala
+#echo "# install scala" | tee -a $bash_rc
+#echo "SCALA_HOME=/usr/share/$scala_file_name" | tee -a $bash_rc
+add_to_bash_rc "#install scala" "SCALA_HOME=/usr/share/scala" 'PATH=$PATH:$SCALA_HOME/bin'
+
+#install conda
+wget $conda_url
+bash $conda_name
+
+#install jupyter
+conda install jupyter
+
+#install wiz_note
+sudo apt-get install wiz_note
+
+#install git
+sudo apt-get install git
